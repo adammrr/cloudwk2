290635e4bcf35a13d03c4d2c060577e2556157e1
Robson <CMPAROBS@ljmu.ac.uk>
2022-01-17 11:12:53 +0000

    Post

--- test.js
+++ test.js
@@ -4,14 +4,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
- res.send('Hello Virtual Machine!')
+    res.send('Hello Virtual Machine!')
 })
 
 app.get('/marco', (req, res) => {
     res.send('Polo!')
-   })
+})
 
+app.post('/name', (req, res) => {
+    var name = req.body.name;
+    console.log("Hello: " + name );
+    res.send('Hello! ' + name + "!");
+})
 
 app.listen(port, () => {
- console.log(`Express Application listening at port 3000`)
+    console.log(`Express Application listening at port 3000`)
 })
