bef14e33b54ef00e338c04e93b0f63403691a7e2
Adam Robson <adammrr@sky.com>
2022-01-24 11:26:39 +0000

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-res.send('Hello Virtual Machine!')
+res.send('Hello Virtual Machine This is Updated!')
 })
 
 app.listen(port, () => {
