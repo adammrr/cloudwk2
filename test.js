d40c231f2fa3cce958ee0ae7fd8b7cc62931f27f
Adam Robson <adammrr@sky.com>
2022-01-24 11:18:22 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,11 @@
-//first NodeJS application
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())
-
 app.get('/', (req, res) => {
-    res.send('Hello Virtual Machine!')
-})
-
-app.get('/marco', (req, res) => {
-    res.send('Polo!')
-})
-
-app.post('/name', (req, res) => {
-    var name = req.body.name;
-    console.log("Hello: " + name );
-    res.send('Hello! ' + name + "!");
+res.send('Hello Virtual Machine!')
 })
 
 app.listen(port, () => {
-    console.log(`Express Application listening at port 3000`)
+console.log(`Express Application listening at port 3000`)
 })
